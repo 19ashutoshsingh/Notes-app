707039c0ad902617583fa91953e0ca0740fd853b
19ashutoshsingh <154892951+19ashutoshsingh@users.noreply.github.com>
2025-04-02 14:23:09 +0530

    added search area

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ProfileInfo from '../../components/Cards/ProfileInfo'
+import Navbar from '../../components/Navbar/Navbar'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
